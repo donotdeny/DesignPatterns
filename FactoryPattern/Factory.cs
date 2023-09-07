e242769d89b8f6d375b3d0d1c67a75ee32aa7ca5
Me <vandung1212001@gmail.com>
2023-09-07 23:11:07 +0700

    Factory Pattern

---
+++ FactoryPattern/Factory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        // private lại không cho bên ngoài gọi linh tinh mà phải gọi qua factory 
+        private Person(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Name: {Name}";
+        }
+
+        // Inner factory để giải quyết vấn đề private constructor
+        public class PersonFactory
+        {
+            // Id auto increase from 0
+            public static int IdPersonFactory { get; set; } = 0;
+            public Person CreatePerson(string name)
+            {
+                return new Person(IdPersonFactory++, name);
+            }
+        }
+    }
+}
---
+++ FactoryPattern/Program.cs
@@ -0,0 +1,9 @@
+﻿using FactoryPattern;
+
+var test = new Person.PersonFactory();
+Person person1 = test.CreatePerson("An");
+Person person2 = test.CreatePerson("Bao");
+Person person3 = test.CreatePerson("Nguyen");
+Console.WriteLine(person1);
+Console.WriteLine(person2);
+Console.WriteLine(person3);
