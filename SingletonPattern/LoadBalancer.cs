27c36cfa3b4959302d35cfd57ef45e61c757122d
Me <vandung1212001@gmail.com>
2023-09-10 23:09:44 +0700

    singleton

---
+++ SingletonPattern/LoadBalancer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    // Class cân bằng tải server 
+    public sealed class LoadBalancer
+    {
+        // singleton implemented
+        private static readonly LoadBalancer _instance = new();
+        private readonly List<Server> _servers;
+        private readonly Random _random = new ();
+
+        private LoadBalancer()
+        {
+            _servers = new List<Server>
+            {
+                new Server{ Name = "ServerI", IP = "120.14.220.18" },
+                new Server{ Name = "ServerII", IP = "120.14.220.19" },
+                new Server{ Name = "ServerIII", IP = "120.14.220.20" },
+                new Server{ Name = "ServerIV", IP = "120.14.220.21" },
+                new Server{ Name = "ServerV", IP = "120.14.220.22" }
+            };
+        }
+
+        public static LoadBalancer GetInstance()
+        {
+            return _instance;
+        }
+
+        public Server NextServer()
+        {
+            int r = _random.Next(_servers.Count);
+            return _servers[r];
+        }
+    }
+
+    public class Server
+    {
+        public string Name { get; set; } = string.Empty;
+        public string IP { get; set; } = string.Empty;
+    }
+}
---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,17 @@
+﻿using SingletonPattern;
+
+var b1 = LoadBalancer.GetInstance();
+var b2 = LoadBalancer.GetInstance();
+var b3 = LoadBalancer.GetInstance();
+var b4 = LoadBalancer.GetInstance();
+// Confirm these are the same instance
+if (b1 == b2 && b2 == b3 && b3 == b4)
+{
+    Console.WriteLine("Same instance\n");
+}
+
+var balancer = LoadBalancer.GetInstance();
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine("Dispatch request to: " + balancer.NextServer().IP);
+}
