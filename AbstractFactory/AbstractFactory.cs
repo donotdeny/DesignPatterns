bb6bb43bd004ff4c99ce29c7e7823ef18b69966e
Me <vandung1212001@gmail.com>
2023-09-11 22:26:30 +0700

    abstract factory

---
+++ AbstractFactory/AbstractFactory.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+
+    // more flexible than factory method by define interface and implement them
+    public interface ICreator
+    {
+        IProductAmisPlatform CreateAmisPlatformProduct();
+        IProductWebsite CreateWebsiteProduct();
+    }
+
+    public class ConcreteCreatorCluster1 : ICreator
+    {
+        public IProductAmisPlatform CreateAmisPlatformProduct()
+        {
+            return new ConcreteProductMily();
+        }
+
+        public IProductWebsite CreateWebsiteProduct()
+        {
+            return new ConcreteProductCommunity();
+        }
+    }
+
+    public class ConcreteCreatorCluster2 : ICreator
+    {
+        public IProductAmisPlatform CreateAmisPlatformProduct()
+        {
+            return new ConcreteProductEducation();
+        }
+
+        public IProductWebsite CreateWebsiteProduct()
+        {
+            return new ConcreteProductForum();
+        }
+    }
+
+    public interface IProductAmisPlatform
+    {
+        string OperationCICD();
+    }
+
+    public class ConcreteProductMily : IProductAmisPlatform
+    {
+        public string OperationCICD()
+        {
+            return "Mily project is continue CI/CD flow...";
+        }
+    }
+
+    public class ConcreteProductEducation : IProductAmisPlatform
+    {
+        public string OperationCICD()
+        {
+            return "Education project is continue CI/CD flow...";
+        }
+    }
+
+    public interface IProductWebsite
+    {
+        string BuildNormal();
+    }
+
+    public class ConcreteProductCommunity : IProductWebsite
+    {
+        public string BuildNormal()
+        {
+            return "Community project is running with normal build...";
+        }
+    }
+
+    public class ConcreteProductForum : IProductWebsite
+    {
+        public string BuildNormal()
+        {
+            return "Forum project is running with normal build...";
+        }
+    }
+}
---
+++ AbstractFactory/Program.cs
@@ -0,0 +1,15 @@
+﻿using AbstractFactory;
+
+ICreator creator = new ConcreteCreatorCluster1();
+IProductAmisPlatform mily = creator.CreateAmisPlatformProduct();
+IProductWebsite community = creator.CreateWebsiteProduct();  
+Console.WriteLine(mily.OperationCICD());
+Console.WriteLine(community.BuildNormal());
+
+Console.WriteLine("-----------------------------");
+
+ICreator creator2 = new ConcreteCreatorCluster2();
+IProductAmisPlatform education = creator2.CreateAmisPlatformProduct();
+IProductWebsite forum = creator2.CreateWebsiteProduct();
+Console.WriteLine(education.OperationCICD());
+Console.WriteLine(forum.BuildNormal());
