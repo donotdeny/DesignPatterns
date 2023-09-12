c7179bc55686c60629f23a62ea5a8eb8afa66374
Me <vandung1212001@gmail.com>
2023-09-12 21:26:16 +0700

    .

---
+++ SingletonPattern/ExcersieUdemy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    public class SingletonTester
+    {
+        public static bool IsSingleton(Func<object> func)
+        {
+            var obj1 = func();
+            var obj2 = func();
+            return ReferenceEquals(obj1, obj2);
+        }
+    }
+}
