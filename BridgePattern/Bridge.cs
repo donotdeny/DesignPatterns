d0748fabb0415f568f69650daa1791f6c79e335b
Me <vandung1212001@gmail.com>
2023-09-20 21:19:30 +0700

    Bridge pattern

---
+++ BridgePattern/Bridge.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgePattern
+{
+    // Lớp trừu tượng định nghĩa giao diện chung cho tất cả các lớp concrete.
+    // Nó cũng có một tham chiếu đến một đối tượng của lớp Implementor.
+    public abstract class Abstraction
+    {
+        protected IImplementation _implementation;
+
+        public Abstraction(IImplementation implementation)
+        {
+            _implementation = implementation;
+        }
+
+        public abstract void Operation();
+    }
+
+    // Lớp refined abstraction mở rộng lớp Abstraction
+    // và cung cấp các phương thức thực hiện cụ thể cho một nhóm lớp concrete khác.
+    public class ExtendedAbstraction : Abstraction
+    {
+        public ExtendedAbstraction(IImplementation implementation) : base(implementation)
+        {
+        }
+
+        public override void Operation()
+        {
+            _implementation.OperationImplementation();
+        }
+    }
+
+    // Interface implementor định nghĩa giao diện cụ thể cho một nhóm lớp concrete.
+    // Nó cũng cung cấp các phương thức thực hiện cho các chức năng được định nghĩa trong lớp Abstraction.
+    public interface IImplementation
+    {
+        void OperationImplementation();
+    }
+
+    // Lớp concrete implementor mở rộng lớp Implementor và cung cấp các phương thức thực hiện cụ thể cho
+    // một nhóm lớp concrete nhất định.
+    public class ConcreteImplementationA : IImplementation
+    {
+        public void OperationImplementation()
+        {
+            Console.WriteLine("ConcreteImplementationA: The result in platform A.\n");
+        }
+    }
+
+    public class ConcreteImplementationB : IImplementation
+    {
+        public void OperationImplementation()
+        {
+            Console.WriteLine("ConcreteImplementationB: The result in platform B.\n");
+        }
+    }
+}
---
+++ BridgePattern/Program.cs
@@ -0,0 +1,9 @@
+﻿using BridgePattern;
+
+IImplementation implementation = new ConcreteImplementationA();
+Abstraction abstraction = new ExtendedAbstraction(implementation);
+abstraction.Operation();
+
+implementation = new ConcreteImplementationB();
+abstraction = new ExtendedAbstraction(implementation);
+abstraction.Operation();
